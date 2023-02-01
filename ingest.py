e9228d37cab895ede67397e199a13b9b7db19d10
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-31 20:38:47 -0700

    Fix format check, make directory argument

--- ingest.py
+++ ingest.py
@@ -18,11 +18,12 @@ class Keybinds:
         self.context = ''
         self.bindings = {}
 
-    def load(self, target: str):
+    def load(self, directory: str, target: str):
         self.context = target.removesuffix('.csv')
-        if re.fullmatch('^[a-zA-Z_]+$') is None:
-            raise InvalidContextException('context names should only contain letters and underscore')
-        with resource.open(target, 'r') as file:
+        if re.fullmatch('^[a-zA-Z_]+$', self.context) is None:
+            raise InvalidContextException(f'Invalid context name: {self.context} Context names should only contain letters and underscore')
+        filepath = os.path.join(directory, target)
+        with open(filepath, 'r') as file:
             reader = csv.reader(file, delimiter=',')
             for index, row in enumerate(reader):
                 if len(row) != 2:
@@ -40,16 +41,16 @@ class ContextSet:
         self.reload_callback = lambda: None
 
     # FIXME: in the event the script is not unloaded this will cause memory usage to balloon
-    def reload(self, folder: str):
-        for filename in os.listdir(folder):
+    def reload(self, directory: str):
+        for filename in os.listdir(directory):
             binding = Keybinds()
             binding.load(filename)
             self.bindings.append(binding)
         self.reload_callback()
 
-    def load(self, folder: str):
-        fs.watch(folder, lambda path, flags: self.reload(folder))
-        self.reload(folder)
+    def load(self, directory: str):
+        fs.watch(directory, lambda path, flags: self.reload(directory))
+        self.reload(directory)
 
     def __iter__(self):
         return self.bindings.__iter__()
