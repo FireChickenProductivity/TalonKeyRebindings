3f43e4e2ae4fead8a43ef563649f5097d305b94f
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-02-05 10:13:08 -0700

    Refresh tag manager on generation

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 
 INPUT_DIRECTORY = os.path.join(compute_file_directory(__file__), 'Key Bindings')
 OUTPUT_DIRECTORY = os.path.join(compute_file_directory(__file__), 'talon_output')
+TAG_MANAGER_FILEPATH = os.path.join(compute_file_directory(__file__), 'tag_manager.py')
 def set_up():
     create_directory_if_nonexistent(INPUT_DIRECTORY)
     create_directory_if_nonexistent(OUTPUT_DIRECTORY)
     context_set = ContextSet()
     builder = TalonBuilder(context_set)
-    generator = TalonGenerator(builder, OUTPUT_DIRECTORY)
+    generator = TalonGenerator(builder, OUTPUT_DIRECTORY, TAG_MANAGER_FILEPATH)
     context_set.load(INPUT_DIRECTORY)
 
 set_up()
