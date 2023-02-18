43128613695d77f7feb9c2e09cf8b36d88154dfd
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-02-18 12:50:39 -0700

    Add tap manager

--- actions.py
+++ actions.py
@@ -7,6 +7,40 @@ insert_delay = module.setting(
     default = 0,
     desc = 'How long to pause between disabling hot keys and inserting the text with the talon_key_rebindings_insert action'
 )
+
+class TapManager:
+    def __init__(self):
+        self.active_tappings = {}
+    
+    def start(self, keystroke: str, interval: float, limit):
+        if keystroke in self.active_tappings:
+            self.stop(keystroke)
+        self.active_tappings[keystroke] = Tapping(keystroke, interval, limit)
+
+    def stop(self, keystroke: str):
+        if keystroke in self.active_tappings:
+            self.active_tappings[keystroke].stop()
+    
+class Tapping:
+    def __init__(self, keystroke: str, interval: float, limit):
+        self.keystroke = keystroke
+        self.interval = interval
+        self.limit = limit
+        self.taps_performed = 0
+        self.job = cron.interval('{interval}ms', self.tap)
+    
+    def tap(self):
+        if self.limit > 0:
+            self.taps_performed += 1
+        if self.taps_performed > self.limit:
+            self.stop()
+        actions.key(self.keystroke)
+    
+    def stop(self):
+        if self.job:
+            cron.cancel(self.job)
+        self.job = None
+
 @module.action_class
 class Actions:
     def talon_key_rebindings_insert(text: str):
@@ -16,3 +50,11 @@ class Actions:
             actions.insert(text)
             actions.mode.enable("hotkey")
         cron.after(f'{insert_delay.get()}ms', lambda: insert_and_reenable_hotkeys(text))
+    
+    def talon_key_rebindings_start_tap(keystroke: str, interval: float, limit: int = 0):
+        ''''''
+        pass
+
+    def talon_key_rebinding_stop_tap(keystroke: str):
+        ''''''
+        pass
