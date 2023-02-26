86352c09167ebcc705cde38629ffc591fce13300
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-02-26 16:28:34 -0700

    Add ability to insert key binds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from .ingest import ContextSet
 from .generator import TalonBuilder, TalonGenerator
 from .fire_chicken.path_utilities import compute_file_directory, create_directory_if_nonexistent
 import os
+from talon import Module
 
 INPUT_DIRECTORY = os.path.join(compute_file_directory(__file__), 'Key Bindings')
 OUTPUT_DIRECTORY = os.path.join(compute_file_directory(__file__), 'talon_output')
@@ -15,3 +16,16 @@ def set_up():
     context_set.load(INPUT_DIRECTORY)
 
 set_up()
+
+module = Module()
+@module.action_class
+class Actions:
+    def keybinder_add_key_bind(text: str, context_name: str = 'main'):
+        ''''''
+        filepath = get_keybinding_filepath(context_name) + '.txt'
+        with open(filepath, 'a') as file:
+            file.write(text)
+            print('appending')
+
+def get_keybinding_filepath(context_name: str):
+    return os.path.join(INPUT_DIRECTORY, context_name)
